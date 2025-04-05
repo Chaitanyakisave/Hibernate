e48e141e5ffe1770da03098054eabce2ce84289e
CTKisave <chaitanyakisave2003@gmail.com>
2025-04-05 14:14:09 +0530

    mycmt

---
+++ SpringDemo1/src/main/java/classes/Demo.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Demo {
+    private String name;
+    private String id ;
+	public Demo(String name, String id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+}
